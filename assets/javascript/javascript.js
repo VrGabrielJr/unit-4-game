1636fa258b909759dee9ee7a9ae120a7b1122fd5
Val Gabriel <gabriel.valryan@hotmail.com>
2018-04-14 08:42:25 -0500

    Completed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,46 +1,50 @@
-// variables
-// variable for player random starting number 19-120
-var startRanNum;
-//variable per button. Also random 1-12 possible function
-var gem1Num;
-var gem2Num;
-var gem3Num;
-var gem4Num;
-// Win and Lose Variables
-var win;
-var lose;
-//total score variable
-var totalScore;
+$( document ).ready(function() {
+    GameStart();
 
-function Random () {
-    gem1Num = Math.floor(Math.random() * 12 + 1) + 1;
-    gem2Num = Math.floor(Math.random() * 12 + 1) + 1;
-    gem3Num = Math.floor(Math.random() * 12 + 1) + 1;
-    gem4Num = Math.floor(Math.random() * 12 + 1) + 1;
-    startRanNum = Math.floor(Math.random() * 120 + 1) + 19;
-
-    console.log(gem1Num);
-    console.log(gem2Num);
-    console.log(gem3Num);
-    console.log(gem4Num);
-    console.log(startRanNum);
-}
-Random();
-
-$("#gem1").click (function () {
-    console.log(gem1Num);
+    $("button").click(function (){
+        var tempVarNum = parseInt(this.value);
+        totalScore += tempVarNum;
+        console.log(tempVarNum);
+        $("#totalValue").text(totalScore);
+        Check();
+    })
 });
+    
+    //Delcare game variables (gem1-gem4 , win , lost, currentScore, GoalScore)
+    $("#gem1").attr("value", Math.floor(Math.random() * 12 + 1) + 1);
+    $("#gem2").attr("value", Math.floor(Math.random() * 12 + 1) + 1);
+    $("#gem3").attr("value", Math.floor(Math.random() * 12 + 1) + 1);
+    $("#gem4").attr("value", Math.floor(Math.random() * 12 + 1) + 1);
+    var startRanNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    var win = 0;
+    var lose = 0;
+    var totalScore = 0;
+    
+    function GameStart() {
+        //Display All stats to DOM
+        startRanNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        totalScore = 0;
+        $("#startValue").text(startRanNum);
+        $("#wins").text("Wins: " + win);
+        $("#lose").text("Lose: " + lose);
+        $("#totalScore").text("Your Total Score is: ");
+        $("#totalValue").text(0);
+    }
 
-$("#gem2").click (function () {
-    console.log(gem2Num);
-});
-
-$("#gem3").click (function () {
-    console.log(gem3Num);
-});
-
-$("#gem4").click (function () {
-    console.log(gem4Num);
-});
+    function Check() {
+        if (totalScore === startRanNum) {
+            win++;
+            $("#wins").text("Wins: " + win);
+            $("totalValue").text("YOU WIN!");
+            GameStart();
+        } 
+        
+        if (totalScore > startRanNum) {
+            lose++;
+            $("#lose").text("Lose: " + lose);
+            $("totalValue").text("YOU LOSE!");
+            GameStart();
+        }
+    };
 
 
